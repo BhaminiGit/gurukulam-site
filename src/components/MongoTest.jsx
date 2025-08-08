589a1fcb6cff43131400c9b19e6b8f668df77e07
BhaminiGit <sbhamini@gmail.com>
2025-08-07 20:33:15 -0500

    initial commit

---
+++ src/components/MongoTest.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+
+const MongoTest = () => {
+
+    const [tasks, setTasks] = useState([{name: "hi", info: "this is default info"}])
+
+  
+    const getAllTasks = async () => {
+      const apiUrl = "http://localhost:5001/api/tasks";
+
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setTasks(data);
+        alert("done")
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      } 
+    };
+
+  
+ 
+    
+  return (
+    <div>
+        <div>MongoTest</div>
+        { tasks.map((task, index) => (
+           <p> { Object.values(task).join("  |  ") } <br/> {index} </p>
+        )
+        )}
+
+        <button onClick={() => getAllTasks}>Get All Tasks</button>
+    </div>
+  )
+}
+
+export default MongoTest
